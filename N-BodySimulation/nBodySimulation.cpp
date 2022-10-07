8bdc8683af25b6cad00440adbe42b8753a49ac53
Juan Pablo <jucarmonam@unal.edu.co>
2022-10-06 22:44:16 -0500

    Simulacion funcional

---
+++ N-BodySimulation/nBodySimulation.cpp
@@ -0,0 +1,187 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+
+#define R_ARGS 1
+#define DIMENSIONS 3
+#define DT 1
+
+/*Variable para el número de particulas*/
+int Nparticles;
+float GravityConstant;
+
+float softening = 1;
+
+struct particle
+{
+	float pos_x = 0, pos_y = 0, vel_x = 0, vel_y = 0, mass = 0;
+};
+
+float dist(sf::Vector2f dif)
+{
+	return sqrt((dif.x * dif.x) + (dif.y * dif.y));
+}
+
+std::random_device rng;
+std::mt19937 dev(rng());
+
+int getRandomInt(int low, int high)
+{
+	std::uniform_int_distribution<> var(low, high);
+	return var(rng);
+}
+
+sf::Vector2f normalise(sf::Vector2f dif)
+{
+	float distance = dist(dif);
+	return sf::Vector2f(dif.x / distance, dif.y / distance);
+}
+
+particle* calculateNewPosition(particle* particles){
+	float ax,ay,az,dx,dy,dz;
+	unsigned int i,j;
+	for (i = 0; i < Nparticles; i++){
+		ax=0.0;
+      	ay=0.0;
+      	//az=0.0;
+		for (j = 0; j < Nparticles; j++)
+		{
+			//recorremos sobre todas las Nparticles "j"
+			dx = particles[j].pos_x - particles[i].pos_x;        
+			dy = particles[j].pos_y - particles[i].pos_y;  
+
+			//dz = *(positions + (j * DIMENSIONS + 2)) - *(positions + (i * DIMENSIONS + 2));
+
+			//matrix that stores 1/r^3 for all particle pairwise particle separations 
+			float inv_r = 1/sqrt(dx*dx + dy*dy + softening*softening);
+			float inv_r3 = pow(inv_r, 3);
+
+			ax = GravityConstant * (dx * inv_r3) * particles[j].mass;
+			ay = GravityConstant * (dy * inv_r3) * particles[j].mass;
+			//az = GravityConstant * (dz * inv_r3) @ mass
+
+			if(i != j){
+				//actualizamos posicion de particula "i"
+				particles[i].pos_x = particles[i].pos_x + DT * particles[i].vel_x + 0.5 * pow(DT,2) * ax; 
+				particles[i].pos_y = particles[i].pos_y + DT * particles[i].vel_y + 0.5 * pow(DT,2) * ay;
+				//particles[j].pos_z = *(positions + (i * DIMENSIONS + 2)) + dt * *(speeds + (i * DIMENSIONS + 2)) + 0.5*dt*dt*az;
+
+				//actualizamos velocidad de particula "i"
+				particles[i].vel_x += DT * ax; 
+				particles[i].vel_y += DT * ay;
+				//particles[i].vel_x += DT * az;
+			}
+		}
+
+	}
+
+	return particles;
+}
+
+int main(int argc,char* argv[])
+{
+    float ax,ay,az,dx,dy,dz;
+
+	//Verificar que la cantidad de argumentos sea la correcta
+    if ((argc - 1) < R_ARGS)
+    {
+        printf("Son necesarios %d argumentos para el funcionamiento\n", R_ARGS);
+        exit(1);
+    }
+
+    Nparticles = atoi(*(argv + 1));
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "N-Body");
+
+    std::cout << "Initializing..." << std::endl;
+
+    window.setFramerateLimit(30);
+
+    auto const screen_width = 500;
+	auto const screen_height = 500;
+
+	GravityConstant = 1;
+
+    unsigned int timer;
+
+    sf::Vector2f mpos;
+
+    sf::CircleShape p_mid(2);
+    //sf::CircleShape p_eff(4);
+	//sf::CircleShape p_eff2(8);
+
+    particle* particles = (particle*)malloc(Nparticles * sizeof(particle));
+
+    for (unsigned int i = 0; i < Nparticles; i++)
+	{
+		int x = getRandomInt(0, screen_width), y = getRandomInt(0, screen_height);
+		particles[i].pos_x = x;
+		particles[i].pos_y = y;
+		particles[i].mass = 100;
+	}
+
+    timer = 400;
+
+
+	float fps;
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        if (timer == 400)
+		{
+			mpos = sf::Vector2f(getRandomInt(screen_width / 5, screen_width / 5 * 4), getRandomInt(screen_height / 5, screen_height / 5 * 4));
+			timer = 0;
+		}
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // Draw particles
+
+		for (unsigned int i = 0; i < Nparticles; i++)
+		{
+			//float green = getRandomInt(25, 75);
+
+			//p_eff.setFillColor(sf::Color(255, 75, 0, green));
+			//p_eff2.setFillColor(sf::Color(255, 75, 0, green / 2));
+
+			p_mid.setPosition(particles[i].pos_x, particles[i].pos_y);
+			//p_eff.setPosition(particles[i].pos_x, particles[i].pos_y);
+			//p_eff2.setPosition(particles[i].pos_x, particles[i].pos_y);
+
+			//particles[i].pos_y += particles[i].vel_y;
+			//particles[i].pos_x += particles[i].vel_x;
+
+			//sf::Vector2f dif = sf::Vector2f(particles[i].pos_x, particles[i].pos_y) - sf::Vector2f(mpos);
+			sf::Vector2f normalised = normalise(sf::Vector2f(particles[i].pos_x, particles[i].pos_y));
+
+			//particles[i].vel_x *= 0.999;
+			//particles[i].vel_y *= 0.999;
+			//particles[i].vel_x -= normalised.x / 20.0f;
+			//particles[i].vel_y -= normalised.y / 20.0f;
+
+			//window.draw(p_eff2);
+			//window.draw(p_eff);
+			window.draw(p_mid);
+		}
+
+		//particles = calculateNewPosition(particles);
+
+        // end the current frame
+        window.display();
+        timer++;
+    }
+
+    return 0;
+}
--- N-BodySimulation/simulation.c
+++ N-BodySimulation/simulation.c
@@ -1,7 +1,8 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#include <CL/cl.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #define R_ARGS 1
 #define DIMENSIONS 3
@@ -15,56 +16,9 @@ int *positions, *newPositions, *speeds;
 /*Arreglo para las masa de cada particula*/
 int *masses;
 
-void nbody_init( int n, cl_float4* pos, cl_float4* vel );
-void nbody_output( int n, cl_float4* pos, cl_float4* vel);
 
 int main(int argc,char* argv[])
 {
-	int step,burst;
-	int nparticle = 8192; 
-	/* MUST be a nice power of two for simplicity */
-	int nparticle2 = nparticle/2;
-	int nstep = 100;
-	int nburst = 20; 
-	/* MUST divide the value of nstep without remainder */
-	int nthread = 64; 
-	/* chosen for ATI Radeon HD 5870 */
-	float dt = 0.0001;
-	float eps = 0.0001;
-	cl_float4* pos1 = (cl_float4*)clmalloc(stdgpu,nparticle*sizeof(cl_float4),0);
-	cl_float4* pos2 = (cl_float4*)clmalloc(stdgpu,nparticle*sizeof(cl_float4),0);
-	cl_float4* vel = (cl_float4*)clmalloc(stdgpu,nparticle*sizeof(cl_float4),0);
-	nbody_init(nparticle,pos1,vel);
-	void* h = clopen(stdgpu,"nbody_kern.cl",CLLD_NOW);
-	cl_kernel krn = clsym(stdgpu,h,"nbody_kern",CLLD_NOW);
-	clndrange_t ndr = clndrange_init1d(0,nparticle,nthread);
-	clarg_set(stdgpu,krn,0,dt);
-	clarg_set(stdgpu,krn,1,eps);
-	clarg_set_global(stdgpu,krn,4,vel);
-	clarg_set_local(stdgpu,krn,5,nthread*sizeof(cl_float4));
-	clmsync(stdgpu,0,pos1,CL_MEM_DEVICE|CL_EVENT_NOWAIT);
-	clmsync(stdgpu,0,vel,CL_MEM_DEVICE|CL_EVENT_NOWAIT);
-
-	for(step=0; step<nstep; step+=nburst) {
-		for(burst=0; burst<nburst; burst+=2) {
-			clarg_set_global(stdgpu,krn,2,pos1);
-			clarg_set_global(stdgpu,krn,3,pos2);
-			clfork(stdgpu,0,krn,&ndr,CL_EVENT_NOWAIT);
-			clarg_set_global(stdgpu,krn,2,pos2);
-			clarg_set_global(stdgpu,krn,3,pos1);
-			clfork(stdgpu,0,krn,&ndr,CL_EVENT_NOWAIT);
-		}
-		clmsync(stdgpu,0,pos1,CL_MEM_HOST|CL_EVENT_NOWAIT);
-		clwait(stdgpu,0,CL_KERNEL_EVENT|CL_MEM_EVENT|CL_EVENT_RELEASE);
-	}
-
-	nbody_output(nparticle,pos1,vel);
-	clclose(stdgpu,h);
-	clfree(pos1);
-	clfree(pos2);
-	clfree(vel); 
-
-	/*
 	float ax,ay,az,dx,dy,dz;
 
 	//Verificar que la cantidad de argumentos sea la correcta
@@ -125,8 +79,6 @@ int main(int argc,char* argv[])
 		*(positions + (i * DIMENSIONS + 1)) = *(newPositions + (i * DIMENSIONS + 1));
 		*(positions + (i * DIMENSIONS + 2)) = *(newPositions + (i * DIMENSIONS + 2));
 	}
-
-	*/
 
 	return 0;
 }
