0c1844617d736370d30a2a3f96d097860ab2162b
Juan Palo Carmona <jucarmonam@unal.edu.co>
2022-11-28 00:54:44 -0500

    script mpi

---
+++ N-BodySimulation/mpi/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PARAM=0
+echo "------------------------------------------------"
+echo "Computación paralela y distribuida - MPI"
+echo "------------------------------------------------"
+echo "Compilando el programa ..."
+sudo mpiCC nBodyMPI.cpp -o nBodyMPI -lsfml-graphics -lsfml-window -lsfml-system
+echo "Compilación terminada, realizando pruebas ..."
+for res in {600,1000,2000}
+do
+    for ((c=1; c<=32; c*=2))
+    do
+        mpirun -np $c --hostfile mpi_hosts ./nBodyMPI $PARAM >> /home/juarodriguezc/results.txt
+    done
+done
+
+
+printf "\n Pruebas terminadas, consulte el archivo 'meansMPI.csv' para un resumen \n "
